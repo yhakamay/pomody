372fa5a6a2e7a09ee3464b3292493f8cd3231c26
Yusuke <yhakamaya@adobe.com>
2022-06-29 17:51:54 +0900

    Implement BreakDurationCard
    Functionality will be implemented after break timer

--- lib/molecules/break_duration_card.dart
+++ lib/molecules/break_duration_card.dart
@@ -9,12 +9,52 @@ class BreakDurationCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const OutlinedCard(
+    return OutlinedCard(
       width: 300,
       height: 120,
-      child: Center(
-        // TODO: Implement
-        child: Text('Break duration'),
+      child: Column(
+        // TODO: Implement after break timer is implemented
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Break',
+            style: TextStyle(fontSize: 20),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 60,
+                child: OutlinedButton(
+                  onPressed: null,
+                  style: OutlinedButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.error,
+                  ),
+                  child: const Icon(Icons.remove),
+                ),
+              ),
+              const SizedBox(
+                width: 120,
+                child: Center(
+                  child: Text(
+                    '${5 ~/ 60}',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 60,
+                child: OutlinedButton(
+                  onPressed: null,
+                  style: OutlinedButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.primary,
+                  ),
+                  child: const Icon(Icons.add),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
