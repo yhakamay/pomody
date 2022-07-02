f060daed713206147d5dd2c9d8e7f539751a025b
Yusuke <yhakamaya@adobe.com>
2022-07-02 17:26:35 +0900

    Use scheduled notifications instead of immediate one
    Fix #25

--- lib/organisms/circular_timer.dart
+++ lib/organisms/circular_timer.dart
@@ -2,7 +2,12 @@ import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+// ignore: depend_on_referenced_packages
+import 'package:timezone/data/latest_all.dart' as tz;
+// ignore: depend_on_referenced_packages
+import 'package:timezone/timezone.dart' as tz;
 
+import '../main.dart';
 import '../providers/timer_provider.dart';
 
 class CircularTimer extends HookConsumerWidget {
@@ -33,36 +38,46 @@ class CircularTimer extends HookConsumerWidget {
         strokeWidth: 12,
         strokeCap: StrokeCap.round,
         textStyle: _textStyle,
-        onStart: _onStart,
+        onStart: () => _onStart(timer),
         onComplete: () => _onComplete(timer),
       ),
     );
   }
 
-  void _onStart() {
-    // Nothing is planned for now
+  void _onStart(TimerController timer) {
+    _scheduleNotification(
+      timer.mode,
+      timer.mode == TimerMode.workTimer
+          ? timer.workDuration
+          : timer.breakDuration,
+    );
   }
 
   Future<void> _onComplete(timer) async {
-    _notify(timer.mode);
     timer.toggleMode();
     timer.start();
   }
 
-  Future<void> _notify(TimerMode mode) async {
-    final flnp = FlutterLocalNotificationsPlugin();
+  Future<void> _scheduleNotification(TimerMode mode, int timerDuration) async {
     const title = 'pomody';
     String body =
         mode == TimerMode.workTimer ? 'Time for a break!' : 'Time to work!';
 
-    await flnp.initialize(
-        const InitializationSettings(iOS: IOSInitializationSettings()));
+    tz.initializeTimeZones();
 
-    flnp.show(
-        0,
-        title,
-        body,
-        const NotificationDetails(
-            iOS: IOSNotificationDetails(presentAlert: true)));
+    flnp.zonedSchedule(
+      0,
+      title,
+      body,
+      tz.TZDateTime.now(tz.local).add(Duration(seconds: timerDuration)),
+      const NotificationDetails(
+        iOS: IOSNotificationDetails(
+          presentAlert: true,
+        ),
+      ),
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+    );
   }
 }
